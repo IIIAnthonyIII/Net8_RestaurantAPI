9a99ce30adbdce7ee986f9f478272c8894daf4d2
IIIAnthonyIII <aadvl382016@outlook.com>
2025-08-04 15:28:21 -0500

    Deploy API en Azure
    Pasos para obtener Azure free por 60 min
    1. Crear una cuenta, sin colocar tarjeta.
    2. Activar el perfil de sandbox.
    https://learn.microsoft.com/en-us/training/modules/create-windows-virtual-machine-in-azure/3-exercise-create-a-vm
    3. Crea una aplicación web
    4. Entre las configuraciones de nombre escoge lo que aparezca, y escoge el plan gratis 60 min.
    5. Después de crear la pagina, ir a configuraciones activar las dos opciones de autenticación:
    * Credenciales de publicación de autenticación básica de SCM y FTP
    6. Vuelve a introducción y descarga el perfil de publicación.
    7. Ir a Visual Studio, click en publicar y escoge "Importa perfil", escoge el archivo y publica.
    8. Para verificar la api anda a lauchSettings del proyecto y agrega esta variable a la página de azure:
    * "ASPNETCORE_ENVIRONMENT": "Development"
    9. En Azure ir opción Variable de entorno y agregar las anteriores.
    10. Reiniciar en introducción e ir a url-del-proyecto/swagger.

--- tests/Restaurants.API.Tests/obj/Debug/net8.0/Restaurants.API.Tests.AssemblyInfo.cs
+++ tests/Restaurants.API.Tests/obj/Debug/net8.0/Restaurants.API.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Restaurants.API.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7364d4fc571ba201501a6ca0cc13448e36433b60")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df61416ed81ef8f34b88414bbccf1755e05ebc68")]
 [assembly: System.Reflection.AssemblyProductAttribute("Restaurants.API.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Restaurants.API.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
