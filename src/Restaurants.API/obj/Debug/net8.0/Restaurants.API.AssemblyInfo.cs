69cd63c9305e56ebca8bd2dc172b3d7913ae28a4
IIIAnthonyIII <aadvl382016@outlook.com>
2025-08-05 15:04:18 -0500

    Create Database in Azure
    1. Crear base de datos
    2. Aplicar oferta gratis, aplicar el recurso learn(free)
    3. Agregar nombre "RestaurantsDb"
    4. Crear un nuevo servidor "restaurantsdbserver", ubicación "Canada central"
    5. Asignar autenticación con sql y escribir los datos
    6. Click en revisar y crear
    7. Después de crear, click en el nombre del servidor para configurar los accesos públicos.
    8. Click en Configuracion de redes, agregar la dirección IP a la cual permitir, marcar el check.

--- src/Restaurants.API/obj/Debug/net8.0/Restaurants.API.AssemblyInfo.cs
+++ src/Restaurants.API/obj/Debug/net8.0/Restaurants.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Restaurants.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df61416ed81ef8f34b88414bbccf1755e05ebc68")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a99ce30adbdce7ee986f9f478272c8894daf4d2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Restaurants.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Restaurants.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- tests/Restaurants.Application.Tests/obj/Release/net8.0/Restaurants.Application.Tests.AssemblyInfo.cs
+++ tests/Restaurants.Application.Tests/obj/Release/net8.0/Restaurants.Application.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Restaurants.Application.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df61416ed81ef8f34b88414bbccf1755e05ebc68")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a99ce30adbdce7ee986f9f478272c8894daf4d2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Restaurants.Application.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Restaurants.Application.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
